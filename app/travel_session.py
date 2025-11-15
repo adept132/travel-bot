4a86a365f213463f579679eee3a24909e3c18f02
adept132 <botsman.d132@gmail.com>
2025-11-15 23:19:53 +0300

    Fix problems with database

--- app/travel_session.py
+++ app/travel_session.py
@@ -1,15 +1,26 @@
-import os
-
-from dotenv import load_dotenv
+﻿import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-load_dotenv()
-user = os.getenv('db_user')
-password = os.getenv('db_password')
-host = os.getenv('db_host')
-name = os.getenv('db_name')
-engine = create_engine(
-    "mysql+pymysql://travel_admin:SQLDataBase132!@localhost:3306/travel_db",
-    echo=True
-)
+
+def get_database_url():
+    cloud_db_url = os.getenv('DATABASE_URL')
+    if cloud_db_url:
+        return cloud_db_url
+    
+    db_host = os.getenv('db_host')
+    if db_host:
+        user = os.getenv('db_user', 'travel_admin')
+        password = os.getenv('db_password', 'SQLDataBase132!')
+        db_name = os.getenv('db_name', 'travel_db')
+        return f'mysql+pymysql://{user}:{password}@{db_host}:3306/{db_name}'
+    
+    return 'sqlite:///travel_bot.db'
+
+database_url = get_database_url()
+
+connect_args = {}
+if 'sqlite' in database_url:
+    connect_args = {'check_same_thread': False}
+
+engine = create_engine(database_url, connect_args=connect_args, echo=True)
 Session = sessionmaker(bind=engine)
