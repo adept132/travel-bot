6c81c9f1e2934bd11cabbf3a53efbd351e7b8355
adept132 <botsman.d132@gmail.com>
2025-11-15 23:01:16 +0300

    Fix config use

--- bot/Traveler_bot.py
+++ bot/Traveler_bot.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from handlers.reminder import send_reminders
 import logging
 from aiogram import Bot, Dispatcher
-from config import token
+from bot.config import token
 from app.travel_session import engine
 from app.travel_database import Base
 from app.travel_scheduler import premium_management_scheduler
